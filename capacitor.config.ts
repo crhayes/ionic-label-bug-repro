06c89314c90d35b37ecc98e14f66c5a6de0a08c9
Chris Hayes <chrisroberthayes@gmail.com>
2023-02-24 01:35:12 -0500

    Add iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'item-sliding-bug-repro',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
